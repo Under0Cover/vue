38b470604c04f24e7428a95a2fad66f720085bc7
Tales Oliver <talis.oliveira@hotmail.com.br>
2022-09-14 09:14:43 -0300

    criar um componente

--- vue-por-microsofot/vue-cli/relecloud/src/main.js
+++ vue-por-microsofot/vue-cli/relecloud/src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import OliverHost from './components/Host.vue'
 
-createApp(App).mount('#app')
+createApp(OliverHost).mount('#app')
+/*
+    UTILIZANDO O EXEMPLO DA MICROSOFT, O VUE RETORNA UM ERRO.
+    EU ENCONTREI UMA SOLUÇÃO NA INTERNET.
+    LINK: https://stackoverflow.com/questions/71205264/component-name-temp-should-always-be-multi-word-vue-multi-word-component-names
+    O ERRO É SIMPLES, PORÉM PODE SER COMPLICADO PARA QUEM ESTÁ COMEÇANDO.
+    BASICAMENTE O RETORNO DO ERRO DIZ QUE O NOME DO SEU COMPONENTE DE SER UMA PALAVRA MÚLTIPLA AO INVÉS DE UMA GENÉRICA.
+    SUBSTITUA A PALAVRA 'HOST' POR OUTRA PALAVRA, NO MEU CASO, 'OLIVERHOST' E FUNCIONARÁ PERFEITAMENTE
+*/
