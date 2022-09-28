92716dcffb9fbdc74a81b041b11de445710c4450
Tales Oliver <talis.oliveira@hotmail.com.br>
2022-09-28 20:57:40 -0300

    corrigindo eslint error

--- udemy_plinio/vue_cli/vue-components-introducao/src/main.js
+++ udemy_plinio/vue_cli/vue-components-introducao/src/main.js
@@ -1,8 +1,7 @@
 import Vue from "vue";
-import App from "./App.vue";
-
-Vue.config.productionTip = false;
+/* eslint-disable */
 
 new Vue({
-  render: (h) => h(App),
-}).$mount("#app");
+  el: "#app",
+  template: "<h1>Vue Components</h1>"
+})
