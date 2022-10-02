3b780311582563cd069b3e3536aec88b41ce65da
Tales Oliver <talis.oliveira@hotmail.com.br>
2022-10-02 17:12:32 -0300

    adicionando appcontador

--- udemy_plinio/vue_cli/boas-praticas-components/src/App.vue
+++ udemy_plinio/vue_cli/boas-praticas-components/src/App.vue
@@ -1,14 +1,18 @@
 <template>
     <AppHeader></AppHeader>
+    <AppContador></AppContador>
 </template>
 
 <script>
 
 import AppHeader from "./AppHeader.vue";
+import AppContador from "./AppContador.vue";
+
 
 export default {
     components: {
-        AppHeader
+        AppHeader,
+        AppContador
     }
 }
 
--- udemy_plinio/vue_cli/boas-praticas-components/src/main.js
+++ udemy_plinio/vue_cli/boas-praticas-components/src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
-// import App from './App.vue'
+import App from './App.vue'
+import AppContador from './AppContador.vue'
 import AppHeader from './AppHeader'
 
 Vue.component('AppHeader', AppHeader)
+Vue.component('AppContador', AppContador);
 
 new Vue ({
-    // render: h => h(App)
+    render: h => h(App)
 }).$mount('#app')
