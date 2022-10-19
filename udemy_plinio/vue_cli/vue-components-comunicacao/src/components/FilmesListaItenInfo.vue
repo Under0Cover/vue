0b8c3879676822df60fb23dd317a1dffc7bff085
Tales Oliver <talis.oliveira@hotmail.com.br>
2022-10-18 21:08:03 -0300

    comunicacao entre components irmaos

--- udemy_plinio/vue_cli/vue-components-comunicacao/src/components/FilmesListaItenInfo.vue
+++ udemy_plinio/vue_cli/vue-components-comunicacao/src/components/FilmesListaItenInfo.vue
@@ -1,12 +1,27 @@
 <template>
     <div>
-        <h2>Filme selecionado</h2>
-
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">Vingadores: Guerra Infinita</h5>
-                <button class="btn btn-danger float-right">Editar</button>
+        <div v-if="filme">
+            <h2>Filme selecionado</h2>
+    
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">{{ filme.titulo }} | {{ filme.ano }}</h5>
+                    <button class="btn btn-danger float-right">Editar</button>
+                </div>
             </div>
         </div>
+        <p v-else></p>
+        <!-- 
+            OPTEI POR NÃO EXIBIR NADA QUANDO O FILME NÃO ESTIVER SELECIONADO
+            VISUALMENTE FICOU COM INFORMAÇÃO DESNECESSÁRIA
+         -->
     </div>
 </template>
+
+<script>
+export default {
+    props: {
+        filme: Object
+    }
+}
+</script>
